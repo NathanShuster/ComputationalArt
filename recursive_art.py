4c98ed7e05ad59fde0d525468a3ee8d60bb20f0c
Oliver Steele <steele@osteele.com>
2017-09-17 14:21:26 -0400

    Format with yapf; use Google docstring conventions

--- recursive_art.py
+++ recursive_art.py
@@ -1,33 +1,43 @@
-""" TODO: Put your header comment here """
+"""TODO: Put your header comment here."""
 
 import random
 from PIL import Image
 
 
 def build_random_function(min_depth, max_depth):
-    """ Builds a random function of depth at least min_depth and depth
-        at most max_depth (see assignment writeup for definition of depth
-        in this context)
+    """Build a random function.
 
+    Builds a random function of depth at least min_depth and depth at most
+    max_depth. (See the assignment write-up for the definition of depth
+    in this context)
+
+    Args:
         min_depth: the minimum depth of the random function
         max_depth: the maximum depth of the random function
-        returns: the randomly generated function represented as a nested list
-                 (see assignment writeup for details on the representation of
-                 these functions)
+
+    Returns:
+        The randomly generated function represented as a nested list.
+        (See the assignment writ-eup for details on the representation of
+        these functions)
     """
     # TODO: implement this
     pass
 
 
 def evaluate_random_function(f, x, y):
-    """ Evaluate the random function f with inputs x,y
-        Representation of the function f is defined in the assignment writeup
+    """Evaluate the random function f with inputs x,y.
 
+    The representation of the function f is defined in the assignment write-up.
+
+    Args:
         f: the function to evaluate
         x: the value of x to be used to evaluate the function
         y: the value of y to be used to evaluate the function
-        returns: the function value
 
+    Returns:
+        The function value
+
+    Examples:
         >>> evaluate_random_function(["x"],-0.5, 0.75)
         -0.5
         >>> evaluate_random_function(["y"],0.1,0.02)
@@ -42,10 +52,13 @@ def remap_interval(val,
                    input_interval_end,
                    output_interval_start,
                    output_interval_end):
-    """ Given an input value in the interval [input_interval_start,
-        input_interval_end], return an output value scaled to fall within
-        the output interval [output_interval_start, output_interval_end].
+    """Remap a value from one interval to another.
 
+    Given an input value in the interval [input_interval_start,
+    input_interval_end], return an output value scaled to fall within
+    the output interval [output_interval_start, output_interval_end].
+
+    Args:
         val: the value to remap
         input_interval_start: the start of the interval that contains all
                               possible values for val
@@ -55,8 +68,11 @@ def remap_interval(val,
                                possible output values
         output_inteval_end: the end of the interval that contains all possible
                             output values
-        returns: the value remapped from the input to the output interval
 
+    Returns:
+        The value remapped from the input to the output interval
+
+    Examples:
         >>> remap_interval(0.5, 0, 1, 0, 10)
         5.0
         >>> remap_interval(5, 4, 6, 0, 2)
@@ -69,12 +85,15 @@ def remap_interval(val,
 
 
 def color_map(val):
-    """ Maps input value between -1 and 1 to an integer 0-255, suitable for
-        use as an RGB color code.
+    """Maps input value between -1 and 1 to an integer 0-255, suitable for use as an RGB color code.
 
+    Args:
         val: value to remap, must be a float in the interval [-1, 1]
-        returns: integer in the interval [0,255]
 
+    Returns:
+        An integer in the interval [0,255]
+
+    Examples:
         >>> color_map(-1.0)
         0
         >>> color_map(1.0)
@@ -90,8 +109,9 @@ def color_map(val):
 
 
 def test_image(filename, x_size=350, y_size=350):
-    """ Generate test image with random pixels and save as an image file.
+    """Generate a test image with random pixels and save as an image file.
 
+    Args:
         filename: string filename for image (should be .png)
         x_size, y_size: optional args to set image dimensions (default: 350)
     """
@@ -110,8 +130,9 @@ def test_image(filename, x_size=350, y_size=350):
 
 
 def generate_art(filename, x_size=350, y_size=350):
-    """ Generate computational art and save as an image file.
+    """Generate computational art and save as an image file.
 
+    Args:
         filename: string filename for image (should be .png)
         x_size, y_size: optional args to set image dimensions (default: 350)
     """
@@ -128,10 +149,10 @@ def generate_art(filename, x_size=350, y_size=350):
             x = remap_interval(i, 0, x_size, -1, 1)
             y = remap_interval(j, 0, y_size, -1, 1)
             pixels[i, j] = (
-                    color_map(evaluate_random_function(red_function, x, y)),
-                    color_map(evaluate_random_function(green_function, x, y)),
-                    color_map(evaluate_random_function(blue_function, x, y))
-                    )
+                color_map(evaluate_random_function(red_function, x, y)),
+                color_map(evaluate_random_function(green_function, x, y)),
+                color_map(evaluate_random_function(blue_function, x, y))
+            )
 
     im.save(filename)
 
